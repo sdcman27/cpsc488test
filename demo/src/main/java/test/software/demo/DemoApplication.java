7b1ce7777505193b715bc3b478c5cd8e3a4b99e1
Seth Chritzman <'>
2023-08-27 15:41:40 -0400

    Punctuation

---
+++ demo/src/main/java/test/software/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package test.software.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public String main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		return "Please Work!!";
+	}
+
+}
